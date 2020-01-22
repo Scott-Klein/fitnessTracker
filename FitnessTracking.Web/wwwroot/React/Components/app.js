0f221aaec94b9a101418fee140d4b606b52deb88
Scott Klein <scotty.klei@gmail.com>
2020-01-22 18:13:02 +1100

    Added mockup react components.

--- FitnessTracking.Web/wwwroot/React/Components/app.js
+++ FitnessTracking.Web/wwwroot/React/Components/app.js
@@ -2,16 +2,76 @@ import Screen from './Screen.js';
 
 function App() {
     return React.createElement(
-        'div',
-        null,
+        "div",
+        { className: "container" },
+        React.createElement(Screen, { toDisplay: "React is working Correctly" }),
+        React.createElement(ExerciseCard, null),
+        React.createElement(FoodCard, null),
+        React.createElement(InfoCard, null)
+    );
+}
+
+function ExerciseCard() {
+    return React.createElement(
+        "div",
+        { className: "card card-body mb-5" },
         React.createElement(
-            'p',
-            null,
-            'Hello ',
-            Math.random(),
-            ' this is React'
+            "h5",
+            { className: "card-title" },
+            "Exercises #1"
         ),
-        React.createElement(Screen, { toDisplay: 'React is working Correctly' })
+        React.createElement(
+            "p",
+            { className: "card-text" },
+            "You have X many pushups to do today"
+        ),
+        React.createElement(
+            "p",
+            { className: "card-text" },
+            "There will be buttons here that let you click to finish sets of exercise"
+        )
+    );
+}
+
+function FoodCard() {
+    return React.createElement(
+        "div",
+        { className: "card card-body mb-5" },
+        React.createElement(
+            "h5",
+            { className: "card-title" },
+            "Food #1"
+        ),
+        React.createElement(
+            "p",
+            { className: "card-text" },
+            "You have so many calories left in your budget."
+        ),
+        React.createElement(
+            "p",
+            { className: "card-text" },
+            "You can eat one apple"
+        )
+    );
+}
+
+function InfoCard() {
+    var today = new Date(Date.now());
+    var now = today.toDateString();
+    return React.createElement(
+        "div",
+        { className: "card card-body mb-5" },
+        React.createElement(
+            "h5",
+            { className: "card-title" },
+            "Info #1"
+        ),
+        React.createElement(
+            "p",
+            { className: "card-text" },
+            "Today is: ",
+            now
+        )
     );
 }
 
--- FitnessTracking.Web/wwwroot/src/React/Components/app.jsx
+++ FitnessTracking.Web/wwwroot/src/React/Components/app.jsx
@@ -2,15 +2,46 @@
 
 function App() {
     return (
-        <div>
-            <p>Hello {Math.random()} this is React</p>
+        <div className="container">
             <Screen toDisplay="React is working Correctly" />
+            <ExerciseCard />
+            <FoodCard />
+            <InfoCard />
         </div>
         );
 }
 
 
+function ExerciseCard() {
+    return (
+        <div className="card card-body mb-5">
+            <h5 className="card-title">Exercises #1</h5>
+            <p className="card-text">You have X many pushups to do today</p>
+            <p className="card-text">There will be buttons here that let you click to finish sets of exercise</p>
+        </div>
+        );
+}
 
+function FoodCard() {
+    return (
+        <div className="card card-body mb-5">
+            <h5 className="card-title">Food #1</h5>
+            <p className="card-text">You have so many calories left in your budget.</p>
+            <p className="card-text">You can eat one apple</p>
+        </div>
+    );
+}
+
+function InfoCard() {
+    const today = new Date(Date.now());
+    const now = today.toDateString();
+    return (
+        <div className="card card-body mb-5">
+            <h5 className="card-title">Info #1</h5>
+            <p className="card-text">Today is: {now}</p>
+        </div>
+    );
+}
 
 ReactDOM.render(
     <App />,
