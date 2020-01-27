ad3ff321e63e9278b90e4fcef52da8c41ce3f077
Scott Klein <scotty.klei@gmail.com>
2020-01-28 10:19:57 +1100

    Integrated Nunit 3 and created test project.

---
+++ FitnessTracker.Tests/HelloNunit.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Tests
+{
+
+    class HelloNunit
+    {
+        [Test]
+        public void TestsNunit()
+        {
+            var varUnderTest = true;
+
+            Assert.That(varUnderTest, Is.EqualTo(true));
+        }
+    }
+}
